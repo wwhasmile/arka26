2e47b5615a52134a9eaacd5c381fc574b6149146
wwhasmile <uwuhasmile@proton.me>
2026-07-28 15:13:07 +0300

    Added AST node types definition

--- gfxc/src/main.c
+++ gfxc/src/main.c
@@ -47,6 +47,9 @@ int main(int argc, char **argv)
 		"";
 	Lexer_Initialize(&lexer, src);
 
+	u32 tokenCount = Lexer_PreCount(&lexer);
+	printf("Valid token count: %u\n", tokenCount);
+
 	LexerToken token = { 0 };
 	do {
 		token = Lexer_Next(&lexer);
