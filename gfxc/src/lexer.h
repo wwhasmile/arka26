cfee0a4e8a6bc80e25c7af594916de0122aa52a0
wwhasmile <uwuhasmile@proton.me>
2026-07-28 11:45:16 +0300

    Lexer definition

--- gfxc/src/lexer.h
+++ gfxc/src/lexer.h
@@ -3,4 +3,17 @@
 
 #include <defines.h>
 
+typedef struct {
+	const char* start;
+	u32 startLine;
+	u32 startColumn;
+
+	const char* cur;
+	u32 curLine;
+	u32 curColumn;
+
+	u32 keywordCount;
+	const char **keywords;
+} Lexer;
+
 #endif // LEXER_H
