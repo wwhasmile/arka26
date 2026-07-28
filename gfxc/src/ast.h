1628f32ff371b69bb1bb7f8f5aff9b2d49496982
wwhasmile <uwuhasmile@proton.me>
2026-07-28 15:34:50 +0300

    Added AST node definition

--- gfxc/src/ast.h
+++ gfxc/src/ast.h
@@ -8,12 +8,15 @@ typedef enum {
 
 	AST_NODE_TYPE_TEXTURE,
 	AST_NODE_TYPE_REGION,
-	AST_NODE_TYPE_CONST,
+	AST_NODE_TYPE_CONSTANT,
+
+	AST_NODE_TYPE_FIELD,
 
 	AST_NODE_TYPE_SCRIPT,
 	AST_NODE_TYPE_LABEL,
 	AST_NODE_TYPE_TIME_LABEL,
 	AST_NODE_TYPE_INSTRUCTION,
+	AST_NODE_TYPE_REGION_REGISTER,
 	AST_NODE_TYPE_INT_REGISTER,
 	AST_NODE_TYPE_FLOAT_REGISTER,
 
@@ -23,9 +26,71 @@ typedef enum {
 	AST_NODE_TYPE_HEX_LITERAL,
 	AST_NODE_TYPE_STRING_LITERAL,
 
-	AST_NODE_TYPE_COMPILER_FUNCTION,
-
 	AST_NODE_TYPE_ENUM_COUNT
 } AstNodeType;
+
+typedef struct {
+	AstNodeType type;
+	u32 line;
+	u32 column;
+	union {
+		struct {
+			u32 fields;
+		} texture;
+		struct {
+			u32 fields;
+		} region;
+		struct {
+			u32 value;
+		} constant;
+		struct {
+			const char *id;
+			u32 idLength;
+			u32 value;
+		} field;
+		struct {
+			const char *id;
+			u32 idLength;
+			u32 statements;
+		} script;
+		struct {
+			const char *id;
+			u32 idLength;
+		} label;
+		struct {
+			f32 offset;
+			bool relative;
+		} timeLabel;
+		struct {
+			const char *id;
+			u32 idLength;
+		} regionRegister;
+		struct {
+			const char *id;
+			u32 idLength;
+		} intRegister;
+		struct {
+			const char *id;
+			u32 idLength;
+		} floatRegister;
+		struct {
+			const char *id;
+			u32 idLength;
+		} identifier;
+		struct {
+			i32 value;
+		} intLiteral;
+		struct {
+			f32 value;
+		} floatLiteral;
+		struct {
+			u32 value;
+		} hexLiteral;
+		struct {
+			const char *data;
+			u32 dataLength;
+		} stringLiteral;
+	} data;
+} AstNode;
 
 #endif // AST_H
