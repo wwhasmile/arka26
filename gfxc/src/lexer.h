4e2d6622eeec11a8636756acff52bf0a728dcbed
wwhasmile <uwuhasmile@proton.me>
2026-07-28 11:50:02 +0300

    Lexer token definition

--- gfxc/src/lexer.h
+++ gfxc/src/lexer.h
@@ -16,4 +16,25 @@ typedef struct {
 	const char **keywords;
 } Lexer;
 
+typedef enum {
+	LEXER_TOKEN_NONE,
+
+	LEXER_TOKEN_DOT,
+	LEXER_TOKEN_COMMA,
+	LEXER_TOKEN_COLON,
+	LEXER_TOKEN_SEMICOLON,
+
+	LEXER_TOKEN_EOF,
+
+	LEXER_TOKEN_ENUM_COUNT
+} LexerTokenType;
+
+typedef struct {
+	LexerTokenType type;
+	u32 line;
+	u32 column;
+	u32 length;
+	const char *lexeme;
+} LexerToken;
+
 #endif // LEXER_H
