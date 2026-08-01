11700b38fb2654c269e0f683592570c67825743e
wwhasmile <uwuhasmile@proton.me>
2026-08-01 23:31:52 +0300

    Stack implementation

--- gfxc/src/stack.c
+++ gfxc/src/stack.c
@@ -1,8 +1,57 @@
 #include "stack.h"
 
-typedef struct Stack {
-	u32 element;
-	u32 capacity;
-	u32 count;
-	u32 _padding;
-} Stack;
+#include <stdlib.h>
+#include <string.h>
+
+#define STACK_DEFAULT_CAPACITY 8
+
+Stack Stack_InitCapacity(u32 elemSize, u32 capacity)
+{
+	Stack result = {
+		NULL,
+		elemSize,
+		capacity,
+		0
+	};
+	return result;
+}
+
+extern inline Stack Stack_Init(u32 elemSize);
+
+u32 Stack_Push(Stack *stack, const void *src)
+{
+	if (stack->ptr == NULL) {
+		stack->count = 0;
+		stack->ptr = malloc(stack->elemSize * stack->capacity);
+	} else if (stack->count == stack->capacity) {
+		u32 capacity = stack->capacity * 2;
+		void *ptr = malloc(stack->elemSize * capacity);
+		memcpy(ptr, stack->ptr, stack->elemSize * stack->capacity);
+		free(stack->ptr);
+		stack->ptr = ptr;
+		stack->capacity = capacity;
+	}
+	memcpy((u8*)stack->ptr + stack->elemSize * stack->count, src, stack->elemSize);
+	return stack->count++;
+}
+
+bool Stack_Pop(Stack *stack, void *dest)
+{
+	if (stack->ptr == NULL || stack->count == 0)
+		return false;
+	memcpy(dest, (u8*)stack->ptr + stack->elemSize * --stack->count, stack->elemSize);
+	return true;
+}
+
+void Stack_Reset(Stack *stack, u32 to)
+{
+	if (stack->count <= to)
+		return;
+	stack->count = to;
+}
+
+void Stack_Release(Stack *stack)
+{
+	if (stack->ptr != NULL)
+		free(stack->ptr);
+}
--- gfxc/src/stack.h
+++ gfxc/src/stack.h
@@ -3,6 +3,30 @@
 
 #include <defines.h>
 
-typedef struct Stack Stack;
+#ifndef STACK_DEFAULT_CAPACITY
+#define STACK_DEFAULT_CAPACITY 8
+#endif // STACK_DEFAULT_CAPACITY
+
+typedef struct Stack {
+	void *ptr;
+	u32 elemSize;
+	u32 capacity;
+	u32 count;
+} Stack;
+
+Stack Stack_InitCapacity(u32 elemSize, u32 capacity);
+
+inline Stack Stack_Init(u32 elemSize)
+{
+	return Stack_InitCapacity(elemSize, STACK_DEFAULT_CAPACITY);
+}
+
+u32 Stack_Push(Stack *stack, const void *src);
+
+bool Stack_Pop(Stack *stack, void *dest);
+
+void Stack_Reset(Stack *stack, u32 to);
+
+void Stack_Release(Stack *stack);
 
 #endif // STACK_H
