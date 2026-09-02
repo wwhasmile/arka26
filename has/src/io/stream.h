db30862fc16776f94d483544a4eb5586d9a0e2cd
wwhasmile <uwuhasmile@proton.me>
2026-09-02 16:47:49 +0300

    Stream naming

--- has/src/io/stream.h
+++ has/src/io/stream.h
@@ -23,58 +23,58 @@ typedef enum {
 typedef struct Stream Stream;
 
 typedef struct {
-	i32 (*read)(Stream *file, void *buffer, u32 count);
-	i32 (*write)(Stream *file, const void *buffer, u32 count);
-	i32 (*seek)(Stream *file, i32 to, StreamWhence whence);
-	i32 (*tell)(Stream *file);
-	i32 (*size)(Stream *file);
-	bool (*close)(Stream *file);
+	i32 (*read)(Stream *stream, void *buffer, u32 count);
+	i32 (*write)(Stream *stream, const void *buffer, u32 count);
+	i32 (*seek)(Stream *stream, i32 to, StreamWhence whence);
+	i32 (*tell)(Stream *stream);
+	i32 (*size)(Stream *stream);
+	bool (*close)(Stream *stream);
 } StreamInterface;
 
-static inline i32 File_Read(Stream *file, void *buffer, u32 count);
-static inline i32 File_Write(Stream *file, const void *buffer, u32 count);
-static inline i32 File_Seek(Stream *file, i32 to, StreamWhence whence);
-static inline i32 File_Tell(Stream *file);
-static inline bool File_Close(Stream *file);
+static inline i32 Stream_Read(Stream *stream, void *buffer, u32 count);
+static inline i32 Stream_Write(Stream *stream, const void *buffer, u32 count);
+static inline i32 Stream_Seek(Stream *stream, i32 to, StreamWhence whence);
+static inline i32 Stream_Tell(Stream *stream);
+static inline bool Stream_Close(Stream *stream);
 
-static inline i32 File_Read(Stream *file, void *buffer, u32 count)
+static inline i32 Stream_Read(Stream *stream, void *buffer, u32 count)
 {
-	StreamInterface *interface = (StreamInterface*)file;
+	StreamInterface *interface = (StreamInterface*)stream;
 	if (interface->read == NULL)
 		return -1;
-	return interface->read(file, buffer, count);
+	return interface->read(stream, buffer, count);
 }
 
-static inline i32 File_Write(Stream *file, const void *buffer, u32 count)
+static inline i32 Stream_Write(Stream *stream, const void *buffer, u32 count)
 {
-	StreamInterface *interface = (StreamInterface*)file;
+	StreamInterface *interface = (StreamInterface*)stream;
 	if (interface->write == NULL)
 		return -1;
-	return interface->write(file, buffer, count);
+	return interface->write(stream, buffer, count);
 }
 
-static inline i32 File_Seek(Stream *file, i32 to, StreamWhence whence)
+static inline i32 Stream_Seek(Stream *stream, i32 to, StreamWhence whence)
 {
-	StreamInterface *interface = (StreamInterface*)file;
+	StreamInterface *interface = (StreamInterface*)stream;
 	if (interface->seek == NULL)
 		return -1;
-	return interface->seek(file, to, whence);
+	return interface->seek(stream, to, whence);
 }
 
-static inline i32 File_Tell(Stream *file)
+static inline i32 Stream_Tell(Stream *stream)
 {
-	StreamInterface *interface = (StreamInterface*)file;
+	StreamInterface *interface = (StreamInterface*)stream;
 	if (interface->tell == NULL)
 		return -1;
-	return interface->tell(file);
+	return interface->tell(stream);
 }
 
-static inline bool File_Close(Stream *file)
+static inline bool Stream_Close(Stream *stream)
 {
-	StreamInterface *interface = (StreamInterface*)file;
+	StreamInterface *interface = (StreamInterface*)stream;
 	if (interface->close == NULL)
 		return -1;
-	return interface->close(file);
+	return interface->close(stream);
 }
 
 #endif // HAS_IO_STREAM_H
